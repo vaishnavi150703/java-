99d009596b8c2290bab0f000ffee6f8468aabbbe
vaishnavi <vaishnaviverma5126@gmail.com>
2023-10-23 14:16:26 +0530

    code

---
+++ pattern5.java
@@ -0,0 +1,13 @@
+public class pattern5 {
+    public static void main(String[] args) {
+        for(int i=5;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
